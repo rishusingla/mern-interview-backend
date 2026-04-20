f8bf98fbce9a9f9ff61899e8aeffeeb166a46832
Rishu Singla <rishusingla289@gmail.com>
2026-04-20 12:03:31 +0530

    "change to dynamic port"

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ import app from './src/app.js'
 import connectDB from './src/config/db.js'
 
 connectDB();
-
-// const PORT=process.env.PORT
+ const PORT=process.env.PORT
 app.listen(3000, () => {
     console.log("Server is running on port no. 3000")
 })
