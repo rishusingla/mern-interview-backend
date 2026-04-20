d59984e237391c8af2525bb47116b3a31c0b429b
Rishu Singla <rishusingla289@gmail.com>
2026-04-20 15:11:39 +0530

    testing performed

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,7 +5,7 @@ import config from '../config/config.js'
 
 async function authMiddleware(req, res,next) {
     const token = req.cookies.token;
-
+        console.log("Cookies:", req.cookies);
     if (!token) {
         return res.status(401).json({
             message:"Token not provided"
